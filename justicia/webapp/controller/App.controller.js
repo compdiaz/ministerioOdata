eba9cd5ec98577e51a18956db5fe7576360cfb8f
Yorman Daniel Diaz Silva <ydiazs@sapimsa.com>
2024-07-10 19:56:27 +0000

    Initial Commit @7/10/2024, 7:56:27 PM

---
+++ justicia/webapp/controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"justicia/controller/BaseController"
+], function(
+	BaseController
+) {
+	"use strict";
+
+	return BaseController.extend("justicia.controller.App", {
+        onInit: function () {
+            
+        }
+	});
+});
